9e91d891affd0e1cdcd19047f7321a7768c0559d
Tan <e2143492@cmaisonneuve.qc.ca>
2025-02-27 11:36:38 -0500

    S6C2 avancement carte

---
+++ gabarit/carte.php
@@ -0,0 +1,21 @@
+<?php 
+/*
+gabarit permettant d'afficher une carte
+*/
+
+?>
+<article class="carte caarte--grande">
+                     <figure class="carte__image">
+                        <img src="images/img1.jpg" alt="Image de voyage">
+                    </figure>
+                    <div class="carte__contenu">
+                        <?php
+                        if(has_post_thumbnail()){
+                            //Permet d'affichr la petite image associé à l'article (image mise en avant)
+                            the_post_thumbnail('thumbnail'); }
+                        ?>
+                    <h2 class="carte__titre"><?php the_title(); ?></h2>
+                    <p class="carte__description"><?php echo wp_trim_words(get_the_content(), 20, "..."); ?></p>
+                    <a class="carte__bouton carte__bouton--actif" href="<?php the_permalink() ?>">Suite</a>
+                </div>
+</article>
