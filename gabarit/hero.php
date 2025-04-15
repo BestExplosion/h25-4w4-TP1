0206682e63bb5615b62af0598d34be1862a6a33a
Tan <e2143492@cmaisonneuve.qc.ca>
2025-04-15 11:30:26 -0400

    création de la carrousel

--- gabarit/hero.php
+++ gabarit/hero.php
@@ -17,6 +17,11 @@
     <div class="hero__carrousel" style="background-image: url('<?php echo $hero_background[0]?>');"></div>
     <div class="hero__carrousel" style="background-image: url('<?php echo $hero_background[1]?>');"></div>
     <div class="hero__carrousel" style="background-image: url('<?php echo $hero_background[2]?>');"></div>
+    <div class="hero__radio">
+        <input class="hero__radio__input" type="radio" name="carrousel" data-id_carrousel="0">
+        <input class="hero__radio__input" type="radio" name="carrousel" data-id_carrousel="1">
+        <input class="hero__radio__input" type="radio" name="carrousel" data-id_carrousel="2">
+    </div>
 <!--////////// HERO__CONTENU //////////-->
         <div class="hero__contenu global">
             <h1 class="hero__titre hero__couleur">
