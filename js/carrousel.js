341ada8a20370d0626c239d035a9b2d0fab15522
Tan <e2143492@cmaisonneuve.qc.ca>
2025-04-17 10:53:08 -0400

    finition de le carrousel

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,33 +1,55 @@
 (function(){
     console.log("carrousel.js");
- 
+
     let heroRadios = document.querySelectorAll(".hero__radio__input");
     let carrousels = document.querySelectorAll(".hero__carrousel");
     let current = 0;
     const total = carrousels.length;
- 
+
     // Initialisation : activer le premier carrousel
     carrousels[current].classList.add("active");
     if (heroRadios[current]) {
         heroRadios[current].checked = true;
     }
- 
-    setInterval(() => {
+
+    // Fonction pour changer le carrousel en fonction de l'index courant
+    function switchCarousel(index) {
         // Retirer la classe active de l'actuel
         carrousels[current].classList.remove("active");
- 
-        // Passer au suivant
-        current = (current + 1) % total;
- 
-        // Ajouter la classe active au nouveau
+        // Passer au nouveau carrousel
+        current = index;
+        // Ajouter la classe active au nouveau carrousel
         carrousels[current].classList.add("active");
- 
         // Cocher le bouton radio correspondant
         if (heroRadios[current]) {
             heroRadios[current].checked = true;
         }
- 
-    }, 5000);
+    }
+
+    // Variable pour stocker l'ID de l'intervalle
+    let interval;
+    // Fonction pour démarrer l'intervalle
+    function startInterval() {
+        // Arrêter l'intervalle précédent s'il existe
+        if (interval) {
+            clearInterval(interval);
+        }
+        // Démarrer un nouvel intervalle
+        interval = setInterval(() => {
+            switchCarousel((current + 1) % total);
+        }, 5000);
+    }
+
+    // Démarrer l'intervalle dès le début
+    startInterval();
+
+    //Changer manuellement le carrousel
+    heroRadios.forEach((radio, index) => {
+        radio.addEventListener("click", () => {
+
+            switchCarousel(index);
+
+            startInterval();
+        });
+    });
 })();
- 
- 
